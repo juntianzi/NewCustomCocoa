0061f2633b4446494f0412cdcf06bb64471a5f42
juntianzi <chenfengde9202@163.com>
2016-01-06 20:14:08 +0800

    new code

---
+++ CocoapodsCode/SomeCustomCode.h
@@ -0,0 +1,14 @@
+//
+//  SomeCustomCode.h
+//  NewCustomCocoapods
+//
+//  Created by caf on 1/6/16.
+//  Copyright © 2016 kkkwan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SomeCustomCode : NSObject
+- (void)lajibokelangfeishijian;
+
+@end
---
+++ CocoapodsCode/SomeCustomCode.m
@@ -0,0 +1,16 @@
+//
+//  SomeCustomCode.m
+//  NewCustomCocoapods
+//
+//  Created by caf on 1/6/16.
+//  Copyright © 2016 kkkwan. All rights reserved.
+//
+
+#import "SomeCustomCode.h"
+
+@implementation SomeCustomCode
+- (void)lajibokelangfeishijian
+{
+    NSLog(@"caonima");
+}
+@end
